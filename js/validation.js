fdb44464c377850695c841630354952dba6c9665
ElizavetaBiryukova <birliza@mail.ru>
2022-03-07 21:57:01 +0200

    Adds validation hashtags

--- js/validation.js
+++ js/validation.js
@@ -1,9 +1,26 @@
+const regExp = /^[а-яА-ЯёЁa-zA-Z0-9]+$/;
+
 const hashtagsInput = document.querySelector('.text__hashtags');
 const commentText = document.querySelector('.text__description');
 
-commentText.addEventListener('input', () => {
-  const valueComment = commentText.value.length;
-  if (valueComment == valueComment.getAttribute('maxlength')) {
-    commentText.setCustomValidity('Комментарии не должны превышать 140 символов');
+hashtagsInput.addEventListener('input', () => {
+  hashtagsInput.setCustomValidity('');
+
+  let inputText = hashtagsInput.value.toLowerCase().trim();
+
+  const arrayHashtags = inputText.split(' ');
+
+  arrayHashtags.forEach(str => {
+    if (str[0] !== '#') {
+      hashtagsInput.setCustomValidity('Хеш-тег должен начинаться с #');
+    }
+    if (str == '#') {
+      hashtagsInput.setCustomValidity('Хеш-тег не может состоять только из одной решётки');
+    }
+  });
+
+  if (inputText.search(regExp) == -1) {
+    hashtagsInput.setCustomValidity('Строка после решётки должна состоять из букв и чисел');
   }
+
 });
